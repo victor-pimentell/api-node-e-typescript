cf887b2dcd7cc535665aada8991fbc08a4e2a96b
Victor Pimentel Correia <victorpimentelw@gmail.com>
2025-08-15 01:47:42 -0300

    Cidade provider implemented

---
+++ src/server/database/providers/cidades/GetById.ts
@@ -0,0 +1,16 @@
+import { ETableNames } from "../../ETableNames";
+import { Knex } from "../../knex";
+import { ICidade } from "../../models";
+
+export const getById = async (id: number): Promise<ICidade | Error> => {
+    try {
+        const result = await Knex(ETableNames.cidade).select('*').where('id', '=', id).first();
+
+        if (result) return result;
+
+        return new Error('Erro ao cadastrar o registro');
+    } catch (error) {
+        console.log(error)
+        return new Error('Erro ao cadastrar o registro');
+    }
+};
